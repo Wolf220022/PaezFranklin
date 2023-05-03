c3e5d6ad9610269f6cca5a00a1efdde08f0cf521
Wolf220022 <105685206+Wolf220022@users.noreply.github.com>
2023-05-02 21:17:40 -0600

    Add files via upload

---
+++ marco-central.php
@@ -0,0 +1,58 @@
+<div style="float:left; width:60%; margin:60px auto; padding:1vw; border-left:1px solid gray; border-right:1px solid gray;">
+   <div  style="width: 100%;  ">     
+    <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
+       <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left; ">
+        	<h1>¿Quién es Páez M. Franklin Xavier.?</h1>
+       		 <hr>
+             
+          <div id="posteo" style="text-align: left; font-size:0.5vw;">
+                Posteado por:  Páez Mendoza<br>
+                Fecha: 2023-05-02 <br>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div style="width: 100%; height:100%;" >
+           <div  style="text-align:justify; font-size: 20px;">
+                   <p class="eys-parrafo">Hola, yo soy Páez Mendoza <b> Franklin Xavier </b>, como estudiante de TICs, he aprendido cómo utilizar herramientas digitales para resolver problemas, mejorar procesos y crear soluciones innovadoras para desafíos en diversas áreas, desde la salud y la educación hasta la industria y el entretenimiento. A través de mi formación, he adquirido habilidades en programación, bases de datos, redes y seguridad informática, entre otros aspectos clave de la tecnología. Me siento orgulloso de pertenecer a una disciplina que está en constante evolución y que tiene el potencial de cambiar el mundo. </p>
+                   <p class="eys-parrafo">Soy un estudiante apasionado por el aprendizaje y el crecimiento personal. Como estudiante, he aprendido a ser perseverante, disciplinado y a encontrar soluciones creativas a los desafíos que se me presentan en el camino. Me encanta explorar nuevos temas, descubrir nuevas perspectivas y expandir mi conocimiento en diversas áreas. Además, me gusta participar en actividades extracurriculares que me permitan desarrollar habilidades sociales, de liderazgo y trabajo en equipo. Espero poder seguir creciendo y aprendiendo durante mi trayectoria como estudiante y, en el futuro, contribuir positivamente en mi comunidad y en la sociedad en general. </p>
+<p class="eys-parrafo"> Mi primer lenguaje de programación fue C++. Elegí C++ porque quería aprender un lenguaje que me permitiera desarrollar aplicaciones de alto rendimiento y que tuviera una sintaxis similar a otros lenguajes de programación. Aunque al principio me costó un poco entender los conceptos de programación orientada a objetos y las estructuras de datos, con el tiempo logré dominar el lenguaje y desarrollar habilidades avanzadas en programación.</p>
+
+   <div id="blog-seccion2"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
+    
+
+
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Eloy Alfaro donde me gradue en el año 2017, de la especialidad Artistico, apenas me gradue coomencé a realizar varios cursos relacionados con la tecnología, después ingrese a la Universidad de Esmeraldas Luis Vargas Torres, donde comencé a trabajar y a estudiar.  </p>
+   <div id="blog-seccion3"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
+    
+
+
+                   <p  class="eys-parrafo">Mi perfil informático se centra en mi capacidad para entender y aplicar conceptos y herramientas relacionados con la tecnología de la información y la informática. Como tal, tengo una sólida comprensión de los fundamentos de la programación y la resolución de problemas, lo que me permite desarrollar aplicaciones y programas eficientes y efectivos. </p> 
+   <div id="blog-seccion4"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
+    
+
+                   <p   class="eys-parrafo">Soy amante a todo lo relacionado con los motores y más a las motos, me gusta ver carreras y nutrirme en todo lo relacionado a las motos.</p>
+       
+
+                   
+
+               <hr>
+               <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
+                  <div style="width: 40%;  float: left;">
+                   <a href="#">&#171;   Previo</a>
+                  </div>
+
+                  <div style="width: 40%; float: right; text-align: right;">
+                   <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
+                  </div>
+                </div>
+           </div>
+  </div> 
+
+  </div>
